79f5d8206d9534869c256a5da3c7cf4992ac4817
latticetower <merlettaia@gmail.com>
2022-02-22 12:48:44 +0100

    add methods for molecules cleaning (split to several, removing the smal ones, etc.)

---
+++ code/common/cleaner.py
@@ -0,0 +1,96 @@
+"""Methods for smiles cleaning and manipulating dataframes
+"""
+import re
+import pandas as pd
+
+square_brackets_regex = re.compile("\[([^\[\]]+)\]")
+ATOM_NAMES = """
+H He Li Be B C N O F Ne Na Mg Al Si P S
+Cl Ar K Ca Sc Ti V Cr Mn Fe Co Ni Cu Zn
+Ga Ge As Se Br Kr Rb Sr Y Zr Nb Mo Tc
+Ru Rh Pd Ag Cd In Sn Sb Te I Xe
+Cs Ba La Hf Ta W Re Os Ir Pt Au Hg Tl Pb Bi
+Po At Rn Fr Ra Ac Rf Ha Sg Ce Pr Nd
+Pm Sm Eu Gd Tb Dy Ho Er Tm Yb Lu Th Pa U Np
+Pu Am Cm Bk Cf Es Fm Md No Lr
+"""
+AROMATIC = "C,N,O,P,S,As,Se,\*".lower().split(",")
+ATOM_NAMES = [name for line in ATOM_NAMES.split("\n") for name in line.split() if len(name) > 0]
+ATOM_NAMES = sorted(ATOM_NAMES, key=len, reverse=True)
+name_regex = "{}|H|C|N|O|S|P|F|Cl|Br|I|{}".format( "|".join(ATOM_NAMES), "|".join(AROMATIC))
+name_regex = re.compile(name_regex)
+
+
+def find_square_brackets(smiles):
+    return square_brackets_regex.findall(smiles)
+
+
+def break_to_parts(smiles):
+    return smiles.split(".")
+
+
+def clean_smiles(smiles):
+    parts = smiles.split(".")
+    parts = list(set(parts))
+    if len(parts) == 1:
+        return ".".join(parts)
+    resulting_parts = []
+    for part in parts:
+        atom_names = name_regex.findall(part)
+        has_carbon = len([name for name in atom_names if name.lower() == "c"])
+        num_atoms = len(atom_names)
+        if has_carbon or num_atoms > 1:
+            resulting_parts.append(part)
+    if len(resulting_parts) == 0:
+        print("Cannot parse", parts)
+        for part in parts:
+            atom_names = name_regex.findall(part)
+            num_atoms = len(set(atom_names))
+            if num_atoms > 2:
+                resulting_parts.append(part)
+        if len(resulting_parts) == 0:
+            resulting_parts = parts
+    smiles_new = ".".join(resulting_parts)
+    return smiles_new
+
+
+def split_df(df, target_col="Active", split_col="parts", index_col="original_index", keep_columns=["Smiles"],
+             renames={"part": "Smiles"}):
+    """splits dataset to submolecules"""
+    def do_transform(x, parts_col="parts", target_col="Active", keep_columns=["Smiles"]):
+        parts = x[parts_col]
+        new_data = {}
+        for i, part in enumerate(parts):
+            prefix = f"part{i}"
+            name = renames.get("part", "part")
+            new_data[f"{prefix}_{name}"] = part
+            new_data[f"{prefix}_num_{split_col}"] = len(parts)
+            if target_col in x:
+                new_data[f"{prefix}_{target_col}"] = x[target_col]
+            for col in keep_columns:
+                if col in x:
+                    name = renames.get(col, col)
+                    new_data[f"{prefix}_{name}"] = x[col]
+        return new_data
+
+    df = df.apply(
+        lambda x: do_transform(
+                x, parts_col=split_col,
+                target_col=target_col, keep_columns=keep_columns
+            ),
+        axis=1,
+        result_type='expand'
+    )
+    col_index = [(x[:x.find("_")+1], x[x.find("_")+1:]) for x in df.columns]
+    df.columns = pd.MultiIndex.from_arrays(zip(*col_index))
+    df = df.stack(0).reset_index().drop('level_1', axis=1).rename(columns={"level_0": index_col})
+    df = df.loc[~df.part.isnull()].reset_index(drop=True)
+    return df
+
+
+def collect_df(df, target_col="Active", split_col="parts", index_col="original_index", split_sep="."):
+    """collects submolecules to dataset"""
+    return df.groupby(index_col).agg({
+        split_col: lambda x: split_sep.join(x),
+        target_col: lambda x: x.any()
+    })
